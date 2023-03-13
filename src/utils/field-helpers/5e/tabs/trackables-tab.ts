c7ab3c802f4cb29bbc198fec85f98185a072f85d
TheDunco <duncvan@gmail.com>
2023-03-13 18:52:42 -0400

    More stuff!

--- src/utils/field-helpers/5e/tabs/trackables-tab.ts
+++ src/utils/field-helpers/5e/tabs/trackables-tab.ts
@@ -1,10 +1,29 @@
 import { Tab } from 'payload/dist/fields/config/types';
 import { option } from '../../../option';
 import { getNSDFields } from '../field-helpers/get-nsd-fields';
+import { statusEffectOptions } from '../options-helpers/status-effects-options';
 
 export const trackablesTab: Tab = {
     label: 'Trackables',
     fields: [
+        {
+            type: 'collapsible',
+            label: 'Status Effects',
+            fields: [
+                {
+                    type: 'array',
+                    name: 'statusEffects',
+                    fields: [
+                        {
+                            type: 'select',
+                            name: 'statusEffect',
+                            options: statusEffectOptions,
+                        },
+                    ],
+                },
+            ],
+        },
+
         {
             type: 'array',
             name: 'trackables',
